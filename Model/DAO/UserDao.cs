a8b8c4f765d07e51a3aec1597c8e6d50d3a7a8b0
Manh Hung <manhhung94qn@gmail.com>
2019-06-26 00:42:08 +0700

    Them tang model

---
+++ Model/DAO/UserDao.cs
@@ -0,0 +1,30 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public bool Login(string username, string pass)
+        {
+            var result = db.Users.Count(x => x.Username == username && x.Password == pass);
+            return result > 0 ? true : false;
+        }
+    }
+}
---
+++ Model/EF/User.cs
@@ -0,0 +1,36 @@
+namespace Model.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public long ID { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [StringLength(32)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
