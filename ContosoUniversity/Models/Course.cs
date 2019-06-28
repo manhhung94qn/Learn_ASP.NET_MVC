3a58a16276c6e05747b28b111c3b116d0b941a8e
Manh Hung <manhhung94qn@gmail.com>
2019-06-28 17:20:37 +0700

    Ngay 28

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]  //Cho phép bạn nhập thuộc tính ID thay vì cơ sở dữ liệu tự động tạo
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
