de29353eb56dd04c0ba038c8e2ca3e04ba1e5a8a
Manh Hung <manhhung94qn@gmail.com>
2019-07-10 00:27:53 +0700

    lay duoc duong dan

--- MyStore/Scripts/ShopScript/main.js
+++ MyStore/Scripts/ShopScript/main.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(document).ready(function(){
+
+    function addProductToLCST(tele){
+        var listCart = JSON.parse(localStorage.getItem("listCart"));
+        if(listCart){
+            alert("hihi")
+        } else {
+            alert("hoho")
+        }
+
+
+    }
+
+    $(".btn-buynow").click( function($event){
+        console.log($(this));
+        let nodeParent  = $(this).parentsUntil($('.parent-node-item').parent()) ;
+        console.log(nodeParent)
+    })
+
+})
+
+
+
+
